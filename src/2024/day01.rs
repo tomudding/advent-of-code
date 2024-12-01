a2712b40c2595739244b0adbbbb8a46fd231522e
Tom Udding <tomudding@users.noreply.github.com>
2024-12-01 06:16:43 +0100

    chore: solved day01
    A lot better than last year, when the first day took me 4 hours as I had zero
    experience with Rust.
    
    ```txt
        Time  Rank  Score       Time  Rank  Score
    00:10:05  4269      0   00:13:24  3790      0
    ```

--- src/2024/day01.rs
+++ src/2024/day01.rs
@@ -8,18 +8,39 @@ use std::io::{self, BufRead};
 use std::path::Path;
 // DO NOT EDIT - DEFAULTS
 
+use std::collections::HashMap;
+
 #[aoc(year = "2024", day = "day01", part = "part1")]
 fn part1() -> String {
-    let (): () = parse_input();
+    let (left_list, right_list): (Vec<i32>, Vec<i32>) = parse_input();
+    let mut left_list: Vec<i32> = left_list;
+    let mut right_list: Vec<i32> = right_list;
 
-    todo!()
+    left_list.sort();
+    right_list.sort();
+
+    let sum: i32 = left_list.iter()
+        .zip(right_list.iter())
+        .map(|(l, r)| (l - r).abs())
+        .sum();
+
+    sum.to_string()
 }
 
 #[aoc(year = "2024", day = "day01", part = "part2")]
 fn part2() -> String {
-    let (): () = parse_input();
+    let (left_list, right_list): (Vec<i32>, Vec<i32>) = parse_input();
+    let mut right_map: HashMap<i32, i32> = HashMap::new();
 
-    todo!()
+    for number in right_list {
+        *right_map.entry(number).or_insert(0) += 1;
+    }
+
+    let sum: i32 = left_list.iter()
+        .map(|&number| number * right_map.get(&number).unwrap_or(&0))
+        .sum();
+
+    sum.to_string()
 }
 
 fn get_file_path() -> String {
@@ -32,16 +53,23 @@ where P: AsRef<Path>, {
     Ok(io::BufReader::new(file).lines())
 }
 
-fn parse_input() -> todo!() {
+fn parse_input() -> (Vec<i32>, Vec<i32>) {
+    let mut left_list: Vec<i32> = Vec::new();
+    let mut right_list: Vec<i32> = Vec::new();
+
     if let Ok(lines) = read_lines(get_file_path()) {
         for line in lines {
             if let Ok(value) = line {
-                todo!();
+                let parts: Vec<i32> = value.split_whitespace()
+                    .map(|s| s.parse().unwrap())
+                    .collect();
+                left_list.push(parts[0]);
+                right_list.push(parts[1]);
             }
         }
     }
 
-    todo!()
+    (left_list, right_list)
 }
 
 #[cfg(test)]
@@ -50,11 +78,11 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(), todo!());
+        assert_eq!(part1(), String::from("11"));
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(), todo!());
+        assert_eq!(part2(), String::from("31"));
     }
 }

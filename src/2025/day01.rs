544fbcffaaba4d62735bcec5a0328ffeec3cea57
Tom Udding <tomudding@users.noreply.github.com>
2025-12-01 06:20:08 +0100

    chore: solved day01
    No global leaderboard this year :(

--- src/2025/day01.rs
+++ src/2025/day01.rs
@@ -3,32 +3,96 @@ use aoc_proc_macros::aoc;
 use aoc_shared_functions::{get_registry, read_lines};
 // END DO NOT EDIT - AOC
 
+enum Direction {
+    Left,
+    Right,
+}
+
 #[aoc(year = "2025", day = "day01", part = "part1")]
 fn part1() -> String {
-    let (): () = parse_input();
+    let rotations: Vec<(Direction, i32)> = parse_input();
+    let mut position: i32 = 50;
+    let mut count_zero: i32 = 0;
 
-    todo!()
+    for (direction, distance) in rotations {
+        position = match direction {
+            Direction::Left => (position - distance).rem_euclid(100),
+            Direction::Right => (position + distance).rem_euclid(100),
+        };
+
+        if position == 0 {
+            count_zero += 1;
+        }
+    }
+
+    count_zero.to_string()
 }
 
 #[aoc(year = "2025", day = "day01", part = "part2")]
 fn part2() -> String {
-    let (): () = parse_input();
+    let rotations: Vec<(Direction, i32)> = parse_input();
+    let mut position: i32 = 50;
+    let mut count_zero: i32 = 0;
 
-    todo!()
+    for (direction, distance) in rotations {
+        count_zero += match direction {
+            Direction::Right => {
+                let mut steps_to_next_zero: i32 = (100 - (position.rem_euclid(100))).rem_euclid(100);
+                if steps_to_next_zero == 0 {
+                    steps_to_next_zero = 100;
+                }
+
+                if steps_to_next_zero > distance {
+                    0
+                } else {
+                    1 + (distance - steps_to_next_zero) / 100
+                }
+            }
+            Direction::Left => {
+                let mut steps_to_next_zero: i32 = position.rem_euclid(100);
+                if steps_to_next_zero == 0 {
+                    steps_to_next_zero = 100;
+                }
+
+                if steps_to_next_zero > distance {
+                    0
+                } else {
+                    1 + (distance - steps_to_next_zero) / 100
+                }
+            }
+        };
+
+        position = match direction {
+            Direction::Left => (position - distance).rem_euclid(100),
+            Direction::Right => (position + distance).rem_euclid(100),
+        };
+    }
+
+    count_zero.to_string()
 }
 
 fn get_file_path() -> String {
     format!("./inputs/2025/day01{}.txt", if cfg!(test) { "-example" } else { "" })
 }
 
-fn parse_input() -> todo!() {
+fn parse_input() -> Vec<(Direction, i32)> {
+    let mut rotations = Vec::new();
+
     if let Ok(lines) = read_lines(get_file_path()) {
         for value in lines.map_while(Result::ok) {
-            todo!();
+            let (dir_char, dist_str) = value.split_at(1);
+            let direction = match dir_char {
+                "L" => Direction::Left,
+                "R" => Direction::Right,
+                _ => panic!("Invalid direction in input"),
+            };
+            let distance: i32 = dist_str.parse().expect("Invalid number");
+
+            rotations.push((direction, distance));
         }
     }
 
-    todo!()
+    rotations
 }
 
 #[cfg(test)]
@@ -37,11 +101,11 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(), todo!());
+        assert_eq!(part1(), "3");
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(), todo!());
+        assert_eq!(part2(), "6");
     }
 }
